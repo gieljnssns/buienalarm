313553571fd923047bdad41cf30f883c15db791a
Giel Janssens <gieljnssns@me.com>
2019-07-28 11:30:42 +0200

    v0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pybuienalarm',
-    version='0.0.2',
+    version='0.0.3',
     packages=['buienalarm'],
     url='https://github.com/gieljnssns/buienalarm',
     license='MIT',
