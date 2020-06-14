4a568b1e68c33dd65338b3edbed97fe1cf0850d5
Giel <gieljnssns@me.com>
2020-06-14 16:22:06 +0200

    Update version

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='pybuienalarm',
-    version='0.0.4',
-    packages=['buienalarm'],
-    url='https://github.com/gieljnssns/buienalarm',
-    license='MIT',
-    author='Giel Janssens',
-    author_email='gieljnssns@vivaldi.net',
-    description='Simple API to access Buienalarm data.',
-    install_requires=['requests']
+    name="pybuienalarm",
+    version="0.0.5",
+    packages=["buienalarm"],
+    url="https://github.com/gieljnssns/buienalarm",
+    license="MIT",
+    author="Giel Janssens",
+    author_email="gieljnssns@vivaldi.net",
+    description="Simple API to access Buienalarm data.",
+    install_requires=["requests"],
 )
